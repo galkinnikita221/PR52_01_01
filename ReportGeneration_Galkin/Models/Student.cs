7321540f5d0ef4fa13b6ceef7693143379216dad
da15632 <vlasove156@gmail.com>
2024-05-29 13:06:33 +0500

    Разработанные модели проекта. Galkin

---
+++ ReportGeneration_Galkin/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGeneration_Galkin.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int IdGroup { get; set; }
+        public bool Expelled{ get; set; }
+        public DateTime DateExpelled { get; set; }
+        public Student(int Id, string FirstName, string LastName, int IdGroup, bool Expelled, DateTime DateExpelled)
+        {
+            this.Id = Id;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.IdGroup = IdGroup;
+            this.Expelled = Expelled;
+            this.DateExpelled = DateExpelled;
+        }
+    }
+}
