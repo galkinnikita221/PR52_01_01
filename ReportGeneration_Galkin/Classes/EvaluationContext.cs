091d8ded44783f0183c6648cc96aad1b86706a5b
Nikita <galkinnikita228@gmail.com>
2024-05-29 18:14:34 +0500

    Разработанные контексты данных для работы с БД. Galkin

---
+++ ReportGeneration_Galkin/Classes/EvaluationContext.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using ReportGeneration_Galkin.Classes.Common;
+using ReportGeneration_Galkin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGeneration_Galkin.Classes
+{
+    public class EvaluationContext : Evaluation
+    {
+        public EvaluationContext(int Id, int IdWork, int IdStudent, string Value, string Lateness) : base(Id, IdWork, IdStudent, Value, Lateness) { }
+
+        public static List<EvaluationContext> AllEvaluations()
+        {
+            List<EvaluationContext> allEvaluations = new List<EvaluationContext>();
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader Evaluation = Connection.Query("Select * From `evaluation` Order By `evaluation`;", connection);
+            while (Evaluation.Read())
+            {
+                allEvaluations.Add(new EvaluationContext(
+                    Evaluation.GetInt32(0),
+                    Evaluation.GetInt32(1),
+                    Evaluation.GetInt32(2),
+                    Evaluation.GetString(3),
+                    Evaluation.GetString(4)));
+            }
+            Connection.CloseConnection(connection);
+            return allEvaluations;
+        }
+    }
+}
---
+++ ReportGeneration_Galkin/Classes/GroupContext.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using ReportGeneration_Galkin.Classes.Common;
+using ReportGeneration_Galkin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGeneration_Galkin.Classes
+{
+    public class GroupContext : Group
+    {
+        public GroupContext(int Id, string Name) : base(Id, Name) { }
+
+        public static List<GroupContext> AllGroups()
+        {
+            List<GroupContext> allGroups = new List<GroupContext>();
+            MySqlConnection connection = Connection.OpenConnection();
+            MySqlDataReader Group = Connection.Query("Select * From `group` Order By `Name`;", connection);
+            while (Group.Read())
+            {
+                allGroups.Add(new GroupContext(
+                    Group.GetInt32(0),
+                    Group.GetString(1)));
+            }
+            Connection.CloseConnection(connection);
+            return allGroups;
+        }
+    }
+}
